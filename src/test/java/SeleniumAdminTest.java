465736ecd7a807891feb48f2a95e32cfbede1c86
MaciejBartos <maciejbartos@outlook.com>
2020-02-09 18:20:35 +0100

    [Change] improved test, integration with selenium on jenkins

--- src/test/java/SeleniumAdminTest.java
+++ src/test/java/SeleniumAdminTest.java
@@ -1,9 +1,15 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
@@ -11,12 +17,12 @@ import static org.testng.Assert.assertNull;
 public class SeleniumAdminTest {
 
     @Test
-    public void ChangeUserRole() {
+    public void ChangeUserRole() throws MalformedURLException {
         System.setProperty("webdriver.chrome.driver", System.getenv("WEBDRIVER"));
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("useAutomationExtension", false);
         options.setAcceptInsecureCerts(true);
-        WebDriver webDriver = new ChromeDriver(options);
+        RemoteWebDriver webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
         webDriver.navigate().to(System.getenv("WM") + "/faces/common/signIn.xhtml");
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[1]/td[2]/input")).sendKeys(System.getenv("LOGINA"));
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input")).sendKeys(System.getenv("PASSWORDA"));
@@ -34,18 +40,21 @@ public class SeleniumAdminTest {
     }
 
     @Test
-    public void increaseStockWarehouse() {
+    public void increaseStockWarehouse() throws MalformedURLException {
         System.setProperty("webdriver.chrome.driver", System.getenv("WEBDRIVER"));
         ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("useAutomationExtension", false);
+//        options.setExperimentalOption("useAutomationExtension", false);
         options.setAcceptInsecureCerts(true);
-        WebDriver webDriver = new ChromeDriver(options);
+
+//        DesiredCapabilities dc = DesiredCapabilities.chrome();
+//        dc.setBrowserName("chrome");
+//        dc.setPlatform(Platform.WINDOWS);
+
+        RemoteWebDriver webDriver = new RemoteWebDriver(new URL("http://localhost:5555/wd/hub"), options);
         webDriver.navigate().to(System.getenv("WM") + "/faces/common/signIn.xhtml");
         //logowanie
-        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[1]/td[2" +
-                "]/input")).sendKeys("JDoe");
-        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[2]/td[2" +
-                "]/input")).sendKeys("P@ssw0rd");
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[1]/td[2" + "]/input")).sendKeys("JDoe");
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[2]/td[2" + "]/input")).sendKeys("P@ssw0rd");
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/p/input")).click();
 
         //dodanie produktu
@@ -61,14 +70,15 @@ public class SeleniumAdminTest {
         webDriver.navigate().to(System.getenv("WM") + "/faces/stock/listStock.xhtml");
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table[1]/tbody/tr/td[2]/select/option[9]")).click();
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table[1]/tbody/tr/td[3]/input")).click();
-        String res =
-                webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table[2]/tbody/tr/td[2]")).getText();
+        String res = webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table[2]/tbody/tr/td[2]")).getText();
         assertEquals("5", res);
 
         //usuniecie produktu
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table[2]/tbody/tr/td[6]/input[1]")).click();
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr[3]/td[2]/input")).sendKeys("5");
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/input[2]")).click();
+
+        webDriver.quit();
 
     }
 }
